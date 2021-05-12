54ebfec724e69b4abb700fed782ed18265d37eaa
raki <rakinniyaram@gmail.com>
2021-05-12 17:13:38 +0900

    Function CRC

--- SRC/CRC.C
+++ SRC/CRC.C
@@ -1,3 +1,22 @@
+/*
+Type defenitions
+*/
+typedef int ui32;
+typedef short int ui16;
+typedef char ui8;
+
+ui16 CRC_16_CCITT(ui8* bitstream)
+{
+	/*
+	Local Variables
+	*/
+	ui32 Polnoml=0x11021;   //1 0001 0000 0010 0001
+	ui32 dividnd=0xFFFF;    //1 1111 1111 1111 111*
+	
+	
+	
+}
+
 void main()
 {
 	//CRC
