52cb62134cc4953ae5d24fdff2290f4d947a73e5
raki <rakinniyaram@gmail.com>
2021-05-13 20:07:01 +0900

    first compile

--- SRC/CRC.C
+++ SRC/CRC.C
@@ -2,7 +2,7 @@
 Preprocessor Defenitions
 */
 #define CRC_INIT (0xFFFF)
-
+#include <stdio.h>
 /*
 Type defenitions
 */
@@ -10,7 +10,7 @@ typedef int ui32;
 typedef short int ui16;
 typedef char ui8;
 
-ui16 CRC_16_CCITT(ui8* bitstream,ui32 len)
+ui16 CRC_16_CCITT(ui8* bitstream,ui32 BYTE_Len)
 {
 	/*
 	Local Variables
@@ -19,8 +19,9 @@ ui16 CRC_16_CCITT(ui8* bitstream,ui32 len)
 	ui8 BYTE,BIT;
 	ui32 Polnoml=0x11021; //1 0001 0000 0010 0001
 	ui32 CRC_buf=CRC_INIT;//1 1111 1111 1111 111*
+	ui32 i=0;
 	
-	for(i=0;i<len;i++)
+	for(i=0;i<BYTE_Len;i++)
 	{
 		BYTE=bitstream[i]; 
 		BYTE_Len=8;
@@ -39,13 +40,12 @@ ui16 CRC_16_CCITT(ui8* bitstream,ui32 len)
 			}
 		}
 	}
-	for(i=
 }
 void main()
 {
 	//CRC
-char data={0xab,0xcd,0xef,0xab,0xcd,0xef};
-ui32 len;
+char data[10]={0xab,0xcd,0xef,0xab,0xcd,0xef};
+ui32 len,CRC;
 
 len=6;
 CRC=CRC_16_CCITT(data,len);
