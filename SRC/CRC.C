eef8c37ab42bf850246ed73999809e5f03f088e1
raki <rakinniyaram@gmail.com>
2021-05-14 02:16:22 +0900

    unit testing 1

--- SRC/CRC.C
+++ SRC/CRC.C
@@ -1,4 +1,11 @@
 /*
+ * CRC_new.c
+ *
+ *  Created on: May 14, 2021
+ *      Author: raki
+ */
+
+/*
 Preprocessor Defenitions
 */
 #define CRC_INIT (0xFFFF)
@@ -6,11 +13,11 @@ Preprocessor Defenitions
 /*
 Type defenitions
 */
-typedef int ui32;
-typedef short int ui16;
-typedef char ui8;
+typedef unsigned int ui32;
+typedef unsigned short int ui16;
+typedef unsigned char ui8;
 
-ui16 CRC_16_CCITT(ui8* bitstream,ui32 len)
+ui32 CRC_16_CCITT(ui8* bitstream,ui32 len)
 {
 	/*
 	Local Variables
@@ -19,47 +26,48 @@ ui16 CRC_16_CCITT(ui8* bitstream,ui32 len)
 	ui32 CRC_buf=CRC_INIT;//1111 1111 1111 1111
 	ui32 BYTE=0,dividnd=0;
 	ui8 BYTE_Len=0,BIT=0,index;
-	
+
 	for(index=0;index<len;index++)
 	{
 		printf("index=%d   ",index);//DEBUG
-		BYTE=bitstream[index]; 
+		BYTE=bitstream[index];
 		printf("BYTE=%x   \n",0x0FF&BYTE);//DEBUG
 		BYTE_Len=8;
 		while(BYTE_Len--)
 		{
-			printf("CRC_buf=%x   ",0xFFFF&CRC_buf);//DEBUG
+			printf("CRC_buf=%x   ",0x1FFFF&CRC_buf);//DEBUG
 			dividnd=(CRC_buf<<1)&0x1FFFF;
 			BIT=( (BYTE>>(BYTE_Len)) &0x1 );
 			printf("BIT=%d   ",BIT);//DEBUG
 			dividnd=dividnd|BIT;
 			printf("dividnd=%x  ",0x01FFFF&dividnd);//DEBUG
 			/*
-			compute 
+			compute
 			*/
-			if((dividnd>>16)==1) 
+			if((dividnd>>16)==1)
 			{
 				CRC_buf=dividnd^Polnoml;
-				printf("XORed CRC_buf=%x   ",0xFFFF&CRC_buf);//DEBUG
+				printf("XORed CRC_buf=%x   ",0x1FFFF&CRC_buf);//DEBUG
 			}
 			else
 			{
-				CRC_buf=(CRC_buf<<1)&0x1FFFF;
+				CRC_buf=dividnd;//CRC_buf=(dividnd<<1)&0x1FFFF;
+				printf("CRC_buf left shift=%x   ",0x1FFFF&CRC_buf);//DEBUG
 			}
 			printf(" \n");
 		}
-		printf("\n",BIT);//DEBUG
+		printf("\n");//DEBUG
 	}
 	for(index=0;index<16;index++)
 	{
-		printf("CRC_buf=%x   ",0xFFFF&CRC_buf);//DEBUG
+		printf("CRC_buf=%x   ",0x1FFFF&CRC_buf);//DEBUG
 		dividnd=CRC_buf<<1;
 		printf("BIT=%d   ",0);//DEBUG
 		printf("dividnd=%x  ",0x01FFFF&dividnd);//DEBUG
-		if((dividnd>>16)==1) 
+		if((dividnd>>16)==1)
 			{
 				CRC_buf=dividnd^Polnoml;
-				printf("XORed CRC_buf=%x   ",0xFFFF&CRC_buf);//DEBUG
+				printf("XORed CRC_buf=%x   ",0x1FFFF&CRC_buf);//DEBUG
 			}
 			else
 			{
@@ -72,10 +80,11 @@ ui16 CRC_16_CCITT(ui8* bitstream,ui32 len)
 void main()
 {
 	//CRC
-unsigned char data[10]={0xa1,0xa2,0xef,0xa6,0x89,0x01};
+unsigned char data[10]={0x41}; //0100 0001
 ui32 len,CRC=0;
 
-len=6;
+len=1;
 CRC=CRC_16_CCITT(data,len);
 printf("CRC=%x",CRC);
 }
+
